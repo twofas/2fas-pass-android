4236a851a3a5b446b7ef7396927a1e681fa67781
Rafał Kobyłko <rafakob@gmail.com>
2025-07-18 00:05:24 +0200

    Handle intent from UserRecoverableAuthException when connecting with Drive

--- data/cloud/src/main/java/com/twofasapp/data/cloud/exceptions/CloudError.kt
+++ data/cloud/src/main/java/com/twofasapp/data/cloud/exceptions/CloudError.kt
@@ -8,6 +8,8 @@
 
 package com.twofasapp.data.cloud.exceptions
 
+import android.content.Intent
+
 sealed class CloudError(val cause: Throwable?) {
     class Unknown(cause: Throwable? = null) : CloudError(cause)
     class AuthenticationError(cause: Throwable? = null) : CloudError(cause)
@@ -17,7 +19,7 @@ sealed class CloudError(val cause: Throwable?) {
     class UpdateFile(cause: Throwable? = null) : CloudError(cause)
     class FileParsing(cause: Throwable? = null) : CloudError(cause)
     class LocalAccountDoesNotExist(cause: Throwable? = null) : CloudError(cause)
-    class NotAuthorized(cause: Throwable? = null) : CloudError(cause)
+    class NotAuthorized(cause: Throwable? = null, val intent: Intent?) : CloudError(cause)
     class WrongBackupPassword(cause: Throwable? = null) : CloudError(cause)
     class FileIsLocked(cause: Throwable? = null) : CloudError(cause)
     class MultiDeviceSyncNotAvailable(cause: Throwable? = null) : CloudError(cause)
