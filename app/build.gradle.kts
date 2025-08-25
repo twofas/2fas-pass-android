fbbdf15d5a748aceb56f156d7e424b28122559ce
Rafał Kobyłko <rafakob@gmail.com>
2025-08-26 00:05:55 +0200

    Bump version 1.2.0 (27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 
     defaultConfig {
         applicationId = "com.twofasapp.pass"
-        versionName = "1.1.6"
-        versionCode = 26
+        versionName = "1.2.0"
+        versionCode = 27
     }
 
     applicationVariants.all {
