b30e0c2ba13382d3f994c4c6a193115020beb018
Rafał Kobyłko <rafakob@gmail.com>
2026-06-01 20:04:21 +0200

    Fix share uri model

--- data/share/src/main/java/com/twofasapp/data/share/domain/ShareItemContent.kt
+++ data/share/src/main/java/com/twofasapp/data/share/domain/ShareItemContent.kt
@@ -22,9 +22,9 @@ internal sealed interface ShareItemContent {
 
         @Serializable
         data class Uri(
-            @SerialName("uri")
+            @SerialName("text")
             val uri: String,
-            @SerialName("match")
+            @SerialName("matcher")
             val match: String?,
         )
     }
