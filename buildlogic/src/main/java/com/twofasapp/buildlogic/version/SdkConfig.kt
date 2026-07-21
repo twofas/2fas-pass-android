722d73665348f2b2e2d549be9c775222d198b372
Rafał Kobyłko <rafakob@gmail.com>
2026-07-21 21:18:35 +0200

    Update libraries and target SDK 37

--- buildlogic/src/main/java/com/twofasapp/buildlogic/version/SdkConfig.kt
+++ buildlogic/src/main/java/com/twofasapp/buildlogic/version/SdkConfig.kt
@@ -10,6 +10,6 @@ package com.twofasapp.buildlogic.version
 
 object SdkConfig {
     const val minSdk = 31
-    const val targetSdk = 36
-    const val compileSdk = 36
+    const val targetSdk = 37
+    const val compileSdk = 37
 }
